5dc55f3fc3b31ccbd022f0a1f168ff2cdb8467ba
mahmadmanzoor <62117177+mahmadmanzoor@users.noreply.github.com>
2025-02-02 03:52:53 +0500

    Dashboard, Events page

--- src/server/routers/auth-router.ts
+++ src/server/routers/auth-router.ts
@@ -18,7 +18,7 @@ export const authRouter = router({
       where: { externalId: auth.id },
     })
 
-    console.log('USER IN DB:', user);
+    console.log("USER IN DB:", user)
 
     if (!user) {
       await db.user.create({
